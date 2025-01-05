7b188e996842d06981f5df6dab80354e05f467ae
Zohaib Arshad <zohaibfta@gmail.com>
2025-01-05 20:01:02 +0500

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import SignUP from "./pages/SignUP";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      {/* for router install pakage (npm i react-router-dom) */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/SignUP" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
