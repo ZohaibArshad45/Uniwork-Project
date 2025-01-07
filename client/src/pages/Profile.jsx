de7f6ce5cf2c0cbcf7bee8f17d8ec11a3eb5a8fd
Zohaib Arshad <zohaibfta@gmail.com>
2025-01-07 08:36:38 +0500

    Upload Profile IMG Incomplete

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,22 +1,58 @@
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux';
+import { useRef } from 'react';
 
 export default function Profile() {
-  const {currentUser} = useSelector((state) => state.user)
-  return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
-        <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg'  />
-        <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg'  />
-        <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg'  />
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>update</button>
+  const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null); // Initialize the fileRef
 
+  const handleFileClick = () => {
+    if (fileRef.current) {
+      fileRef.current.click(); // Trigger the file input click event
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        {/* File input */}
+        <input type="file" ref={fileRef} hidden accept='image/*'  />
+        <img
+          onClick={handleFileClick} // Attach click handler
+          src={currentUser?.avatar || 'default-avatar.png'} // Provide a default avatar if undefined
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+
+        {/* Other inputs */}
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
       </form>
+
+      {/* Account management */}
       <div className="flex justify-between mt-5">
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
     </div>
-  )
+  );
 }
